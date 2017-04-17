84f4b240ef1092432f3c92a0108ade31d8eac169
Your Name <you@example.com>
2017-04-17 19:54:03 +0200

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = test.concat(femaleNames, maleNames)
+
+var newName = 'Marian' {
+    if (allNames.indexOf(newName) === -1) {
+        push(allNames);
+    }
+}
